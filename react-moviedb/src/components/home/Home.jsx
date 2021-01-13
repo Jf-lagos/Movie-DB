ffc723e66cf1e5c97e1e407427d205ff3445749f
Jeff <lagosjf@gmail.com>
2021-01-13 22:26:14 +0100

    second

--- react-moviedb/src/components/home/Home.jsx
+++ react-moviedb/src/components/home/Home.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { fetchMovies } from "../../service";
+import { fetchMovies, fetchGenre } from "../../service";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 //import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css"
 
 export function Home() {
     const [nowPlaying, setNowPlaying] = useState([]);
+    const [genres, setGenres] = useState([]);
 
     useEffect (() =>{
         const fetchAPI = async () => {
             setNowPlaying(await fetchMovies());
+            setGenres(await fetchGenre());
         };
 
         fetchAPI()
@@ -32,12 +34,35 @@ const movies = nowPlaying.slice(0, 5).map((item, index) => {
     );
  });
 
+ const genreList = genres.map((item, index)=> {
+     return (
+         <li className="list-inline-item" key={index}>
+             <button type="button" className="btn btn-outline-info">
+                {item.name}
+             </button>
+         </li>
+     )
+ })
+
   return (
       <div className="container">
           <div className="row">
             <div className="col">
-            <Carousel>{movies}</Carousel>
+            <Carousel
+                autoPlay={true}
+                pauseOnvisibility={true}
+                slidesshowSpeed={5000}
+                version={4}
+                indicators={false}
+            >{movies}</Carousel>
             </div>
+          </div>
+          <div className="row mt-3">
+              <div className="col">
+                    <ul className="list-inline">
+                    {genreList}
+                    </ul>
+              </div>
           </div>
       </div>
   );
